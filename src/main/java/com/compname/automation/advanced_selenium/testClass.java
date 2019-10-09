18e49efabafb911d5ae0241666475c30d47103bc
keeplearn1 <keeplearn247@gmail.com>
2019-10-09 14:36:22 +0530

    advanced sel project started

---
+++ src/main/java/com/compname/automation/advanced_selenium/testClass.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.compname.automation.advanced_selenium;
+
+/**
+ * @author balaji
+ * @Creation_Date:  Oct 9, 2019 
+ * @ClassName: testClass.java
+ * @LastModified_Date:  Oct 9, 2019 
+ */
+public class testClass {
+
+}
---
+++ src/test/java/com/compname/automation/advanced_selenium/sampleTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.compname.automation.advanced_selenium;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author balaji
+ * @Creation_Date:  Oct 9, 2019 
+ * @ClassName: sampleTest.java
+ * @LastModified_Date:  Oct 9, 2019 
+ */
+public class sampleTest {
+
+	
+	@Test
+	public void sampletestforLogin(){
+		
+		System.out.println("sampl test");
+	}
+	
+}
